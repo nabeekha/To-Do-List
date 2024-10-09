109dec23c17238499aa6232ace147859b8cd407b
Nabeel Khan <nabeekha@umich.edu>
2024-10-08 22:44:51 -0400

    adding main screen buttons

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const landingButton = document.getElementById("landing-page-button");
 const hero = document.getElementById("hero-section");
 const tracker = document.getElementById("task-tracker-section")
+const backButton = document.getElementById("go-back");
 
 // function to show tracker after button is clicked
 landingButton.addEventListener("click", () => {
     hero.classList.add('hidden')
     tracker.classList.remove('hidden')
+})
+
+backButton.addEventListener("click", () => {
+    hero.classList.remove('hidden')
+    tracker.classList.add('hidden')
 })
