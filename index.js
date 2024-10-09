464bfa3b2476db5d3c88f794bf9a1417b21f6dc3
Nabeel Khan <nabeekha@umich.edu>
2024-10-08 22:21:11 -0400

    adding ability to hide classes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const landingButton = document.getElementById("landing-page-button");
-const hero = document.getElementById("hero");
-const tracker = document.getElementById("tracker")
+const hero = document.getElementById("hero-section");
+const tracker = document.getElementById("task-tracker-section")
 
 // function to show tracker after button is clicked
 landingButton.addEventListener("click", () => {
-    hero.classList.add(hidden)
-    tracker.classList.remove(hidden)
+    hero.classList.add('hidden')
+    tracker.classList.remove('hidden')
 })
